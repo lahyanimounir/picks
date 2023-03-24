6b15a6ed3cbcf5768c85ade9a7530afe51588018
lahyani mounir <lahyani.mo@gmail.com>
2023-03-24 22:08:48 +0000

    tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  choice:string = 'Ongoing'
+  segments:string = 'Hotels'
+  
+  halfWidth = 0
 
-  constructor() {}
+  constructor() {
+    this.halfWidth = window.screen.width / 2
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.halfWidth = window.screen.width / 2
+  }
 
 }
